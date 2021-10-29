10f5a865910c822a19c0870dedce2463234462c2
Carlos Sosa <carlangas10001@gmail.com>
2021-10-29 13:17:11 -0500

    arreglando bug de multiple click en slider

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,32 +2,50 @@ export default function ({ carousel, nextBtn, prevBtn }) {
   const $carousel = document.querySelector(carousel);
   const $nextBtn = document.querySelector(nextBtn);
   const $prevBtn = document.querySelector(prevBtn);
+
+  let isClicked = false;
   // Se pone el ultimo slide al inicio, por si le dan click al prevBtn
   $carousel.insertAdjacentElement("afterbegin", $carousel.lastElementChild);
 
   const nextSlide = () => {
-    $carousel.style.transition = "margin 0.5s";
-    $carousel.style.marginLeft = "-200%";
-    setTimeout(() => {
-      $carousel.style.transition = null;
-      $carousel.insertAdjacentElement("beforeend", $carousel.firstElementChild);
-      $carousel.style.marginLeft = "-100%";
-    }, 500);
+    if (!isClicked) {
+      isClicked = true;
+      $carousel.style.transition = "margin 0.5s";
+      $carousel.style.marginLeft = "-200%";
+      setTimeout(() => {
+        $carousel.style.transition = null;
+        $carousel.insertAdjacentElement(
+          "beforeend",
+          $carousel.firstElementChild
+        );
+        $carousel.style.marginLeft = "-100%";
+        isClicked = false;
+      }, 500);
+    }
   };
 
   const prevSlide = () => {
-    $carousel.style.transition = "margin 0.5s";
-    $carousel.style.marginLeft = "0";
-    setTimeout(() => {
-      $carousel.style.transition = null;
-      $carousel.insertAdjacentElement("afterbegin", $carousel.lastElementChild);
-      $carousel.style.marginLeft = "-100%";
-    }, 500);
+    if (!isClicked) {
+      isClicked = true;
+      $carousel.style.transition = "margin 0.5s";
+      $carousel.style.marginLeft = "0";
+      setTimeout(() => {
+        $carousel.style.transition = null;
+        $carousel.insertAdjacentElement(
+          "afterbegin",
+          $carousel.lastElementChild
+        );
+        $carousel.style.marginLeft = "-100%";
+        isClicked = false;
+      }, 500);
+    }
   };
 
   document.addEventListener("click", (e) => {
-    if (e.target === $nextBtn) nextSlide();
-    else if (e.target === $prevBtn) prevSlide();
-    else return false;
+    if (e.target === $nextBtn) {
+      nextSlide();
+    } else if (e.target === $prevBtn) {
+      prevSlide();
+    } else return false;
   });
 }
